ecb62dc0d0b58bf67af115c86131e0a0a255b563
pooja patil <poojaspatil4322@gmail.com>
2025-07-01 16:09:08 +0530

    logoutTeacher

--- src/main/java/Layer/NewStudentManagement/Controller/TeacherController.java
+++ src/main/java/Layer/NewStudentManagement/Controller/TeacherController.java
@@ -5,9 +5,14 @@ import Layer.NewStudentManagement.Entity.StudentTeacher;
 import Layer.NewStudentManagement.Security.LoginRequest;
 import Layer.NewStudentManagement.Security.LoginResponse;
 import Layer.NewStudentManagement.Service.TeacherService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -85,4 +90,14 @@ public class TeacherController
     public ResponseEntity<String> resetPassword(@RequestBody ResetPasswordRequest request) {
         return ResponseEntity.ok(teacherService.resetPassword(request.getEmail(), request.getOtp(), request.getNewPassword()));
     }
+
+    @GetMapping("/teacherLogout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok("Logout successful");
+    }
+
 }
