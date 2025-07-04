4d290b7aac8e13973a0ddd9c77e336f3904760f9
pooja patil <poojaspatil4322@gmail.com>
2025-07-04 10:47:50 +0530

    security

--- src/main/java/Layer/NewStudentManagement/Security/SecurityConfig.java
+++ src/main/java/Layer/NewStudentManagement/Security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/stafflogin","/teacherLogin").permitAll()
+                        .requestMatchers("/stafflogin","/teacherLogin","/verifyOtpToTeacher","/sendOtpToTeacher","/resetTeacherPassword").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
