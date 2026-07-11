a1e77bfc00f41f98824b05aba497ce6fff903608
ssk607 <sidkhatavkar607@gmail.com>
2026-07-11 17:16:32 +0530

    Scheduler logic added but commented as of now due to unknown aws ec2 consumption -> charges

--- src/main/java/Layer/NewStudentManagement/Entity/DatabaseCleanupScheduler.java
+++ src/main/java/Layer/NewStudentManagement/Entity/DatabaseCleanupScheduler.java
@@ -1,6 +1,6 @@
 //package Layer.NewStudentManagement.Entity;
 //
-//import Layer.NewStudentManagement.Service.FeesService;
+//import Layer.NewStudentManagement.Service.NotificationService;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.scheduling.annotation.Scheduled;
 //import org.springframework.stereotype.Component;
@@ -8,19 +8,22 @@
 //@Component
 //public class DatabaseCleanupScheduler {
 //
-//    @Autowired
-//    FeesService feesService;
+////    @Autowired
+////    FeesService feesService;
 //
-//    // ⚡ Configuration option A: Runs exactly every 5 seconds (5000 milliseconds)
-////    @Scheduled(fixedRate = 5000)
-////    public void clearExpiredSessions() {
-////        System.out.println("Fixed Rate Task Executing at: " + LocalDateTime.now());
-////    }
+//    @Autowired
+//    NotificationService notificationService;
 //
 //    // ⏱️ Configuration
 ////    @Scheduled(cron = "0 0 0 * * ?")//option B: Runs daily at 12:00 AM (Midnight) using a Cron Expression
-//    @Scheduled(cron = "0 */1 * * * ?")//option B: Runs daily at 12:00 AM (Midnight) using a Cron Expression
+////    @Scheduled(cron = "0 */1 * * * ?")//option B: Runs daily at 12:00 AM (Midnight) using a Cron Expression
+////    public void generateDailyFeeReports() {
+////        feesService.getFeesDueInDays(7);
+////    }
+//
+////    @Scheduled(fixedRate = 5000) //for testing runs every 5 secs
+//    @Scheduled(cron = "0 0 0 * * ?")//option B: Runs daily at 12:00 AM (Midnight) using a Cron Expression
 //    public void generateDailyFeeReports() {
-//        feesService.getFeesDueInDays(7);
+//        notificationService.deleteOlderNotification();
 //    }
 //}
